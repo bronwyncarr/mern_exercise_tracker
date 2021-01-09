5b96f3017442063a58645b056adb2732337ecab0
bronwyncarr <bronwynacarr@gmail.com>
2021-01-09 22:55:10 +1100

    adds individual get, update, delete route

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -11,7 +11,7 @@ router.route("/").get((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
-//End point for post requests to /add
+// End point for a post requests to /add
 // Creates new exericse with info and saves it's to the db.
 // Error handling if 400 Error occurs.
 router.route("/add").post((req, res) => {
@@ -31,6 +31,36 @@ router.route("/add").post((req, res) => {
     .save()
     .then(() => res.json("Exercise added!"))
     .catch((err) => res.status(400).json("Error: " + err));
+});
+
+// End point for a get request for each individual exercise.
+router.route('/:id').get((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// End point for a delete request to delete each individual exercise.
+router.route('/:id').delete((req, res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Exercise deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// End point for post request to update a individual existing exercise.
+router.route('/update/:id').post((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => {
+      exercise.username = req.body.username;
+      exercise.description = req.body.description;
+      exercise.duration = Number(req.body.duration);
+      exercise.date = Date.parse(req.body.date);
+
+      exercise.save()
+        .then(() => res.json('Exercise updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
